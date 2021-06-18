2776450cc963ed37e3e5355829c1eb8c19cf1d79
fabiana farias <farias.fabianam@gmail.com>
2021-06-17 21:46:39 -0300

    Concluindo a quarta activity

--- app/src/main/java/com/fabianafarias/serasaproway/Activity3.kt
+++ app/src/main/java/com/fabianafarias/serasaproway/Activity3.kt
@@ -1,35 +1,59 @@
 package com.fabianafarias.serasaproway
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import java.util.*
 
 class Activity3 : AppCompatActivity() {
+    lateinit var btnSimulation:Button
+    lateinit var valueEditText: EditText
+    lateinit var rendaEditText: EditText
+    lateinit var nameEditText: EditText
+    lateinit var birthDateEditText: EditText
+    lateinit var phoneNumberEditText: EditText
+    lateinit var emailEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_3)
 
-        var etValue = findViewById<EditText>(R.id.et_value)
-        var etRenda = findViewById<EditText>(R.id.et_renda)
-        var etName = findViewById<EditText>(R.id.et_name)
-        var etBirthDate = findViewById<EditText>(R.id.et_birth_date)
-        var etPhoneNumber = findViewById<EditText>(R.id.et_phone_number)
-        var etEmail = findViewById<EditText>(R.id.et_email)
+        var valueEditText = findViewById<EditText>(R.id.et_value)
+        var rendaEditText = findViewById<EditText>(R.id.et_renda)
+        var nameEditText = findViewById<EditText>(R.id.et_name)
+        var birthDateEditText = findViewById<EditText>(R.id.et_birth_date)
+
+        //Calendar
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        birthDateEditText.setOnClickListener {
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in TextView
+                birthDateEditText.setText("" + dayOfMonth + "/" + month + "/" + year)
+            }, year, month, day)
+            dpd.show()
+
+        var phoneNumberEditText= findViewById<EditText>(R.id.et_phone_number)
+        var emailEditText = findViewById<EditText>(R.id.et_email)
         var btnSimulation = findViewById<Button>(R.id.btn_simulation)
 
         btnSimulation.setOnClickListener {
 
             val intent = Intent(this, Activity4::class.java)
 
-            val value = etValue.text
-            val renda = etRenda.text
-            val name = etName.text
-            val birthDate = etBirthDate.text
-            val phoneNumber = etPhoneNumber.text
-            val email = etEmail.text
+            val value = valueEditText.text.toString()
+            val renda = rendaEditText.text.toString()
+            val name = nameEditText.text.toString()
+            val birthDate = birthDateEditText.text.toString()
+            val phoneNumber = phoneNumberEditText.text.toString()
+            val email = emailEditText.text.toString()
 
             intent.putExtra("Valor", value)
             intent.putExtra("Renda", renda)
@@ -39,6 +63,7 @@ class Activity3 : AppCompatActivity() {
             intent.putExtra("Email", email)
 
             startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/fabianafarias/serasaproway/Activity4.kt
+++ app/src/main/java/com/fabianafarias/serasaproway/Activity4.kt
@@ -1,91 +1,40 @@
 package com.fabianafarias.serasaproway
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 
-class Activity4 : AppCompatActivity(), AdapterView.OnItemClickListener  {
-
-    private var listView2:ListView ? =null
-    private var itemAdapters2:ItemAdapters2 ? =null
-    private var arrayList2:ArrayList<ItemList2> ? =null
-
+class Activity4 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_4)
 
-        listView2 = findViewById(R.id.cardview_list_view)
-        arrayList2 = ArrayList()
-        arrayList2 = setDataItem2()
-        itemAdapters2 = ItemAdapters2(applicationContext, arrayList2!!)
-        listView2?.adapter = itemAdapters2
-        listView2?.onItemClickListener = this
+        val value = intent.getStringExtra("Valor")
+        val valueTextView: TextView = findViewById(R.id.tv_value_inserted)
+        valueTextView.text = value
 
-    }
+        val renda = intent.getStringExtra("Renda")
+        val rendaTextView: TextView = findViewById(R.id.tv_renda_iserted)
+        rendaTextView.text = renda
 
-    private fun setDataItem2() : ArrayList<ItemList2> {
+        val name = intent.getStringExtra("Nome")
+        val nameTextView: TextView = findViewById(R.id.tv_name_inserted)
+        nameTextView.text = name
 
-        var listItem2:ArrayList<ItemList2> = ArrayList()
+        val birthDate = intent.getStringExtra("Data de Nascimento")
+        val birthDateTextView: TextView = findViewById(R.id.tv_birth_date_inserted)
+        birthDateTextView.text = birthDate
 
-        listItem2.add(
-            ItemList2(
+        val phoneNumber = intent.getStringExtra("Telefone")
+        val phoneNumberTextView: TextView = findViewById(R.id.tv_phone_number_inserted)
+        phoneNumberTextView.text = phoneNumber
 
-                "Nome completo:",
-                null
-            )
-        )
-
-        listItem2.add(
-            ItemList2(
-
-                "Data de nascimento:",
-                null
-            )
-        )
-
-        listItem2.add(
-            ItemList2(
-
-                "Renda mensal:",
-                null
-            )
-        )
-
-        listItem2.add(
-            ItemList2(
-
-                "Valor solicitado:",
-                null
-            )
-        )
-
-        listItem2.add(
-            ItemList2(
-
-                "Email:",
-                null
-            )
-        )
-
-        listItem2.add(
-            ItemList2(
-
-                "Telefone:",
-                null
-            )
-        )
-
-        return listItem2
-
+        val email = intent.getStringExtra("Email")
+        val emailTextView: TextView = findViewById(R.id.tv_email_inserted)
+        emailTextView.text = email
     }
 
 
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        var items2:ItemList2 = arrayList2?.get(position)!!
-        val intent2 = Intent(this, Activity4::class.java)
-        startActivity(intent2)
-    }
+
 }
